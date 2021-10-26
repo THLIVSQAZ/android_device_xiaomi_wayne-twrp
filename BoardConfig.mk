845fa0b633dfe36cf8f904abc5b17e72d1ed686f
THLIVSQAZ <zgmzzzz18@gmail.com>
2021-10-26 17:21:58 +0800

    wayne: Disable encrypt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,6 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
-TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
 TW_IGNORE_MISC_WIPE_DATA := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
